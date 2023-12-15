c6aa4af5f8ab948b04d1757e6a9f1289229c3b5d
unknown <mahmudul.cse.eng@gmail.com>
2023-12-16 00:24:06 +0600

    header added

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,10 +1,12 @@
+import moment from 'moment';
 import logo from '../../../assets/images/logo.png';
 
 const Header = () => {
     return (
-        <div>
-            <img src={logo} alt="" />
+        <div className='text-center '>
+            <img className='mx-auto' src={logo} alt="" />
             <p>Journalism without Fear and Favour</p>
+            <p className='text-3xl'>{moment().format('dddd, MMMM D, YYYY') }</p>
             
         </div>
     );
